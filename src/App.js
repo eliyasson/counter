e09d51b40cff736dd091f82c898e8ae9d610f3cd
Eliyas Kassaye <eliyasson11@gmail.com>
2023-01-29 07:34:06 +0200

    add reset button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h2>{value}</h2>
       <button className="btn" onClick={() => setValue(value + 1)}>Increament</button>
       <button className="btn" onClick={() => setValue(value - 1)} disabled={value === 0} >Decreament</button>
+      <button className="btn" onClick={() => setValue(0)}>Reset</button>
     </div>
   )
   
